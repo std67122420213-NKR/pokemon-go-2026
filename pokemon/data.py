ddcd495727fec835b3d63ab0fe18b87cb1d02489
std67122420213-NKR <std.67122420213@ubru.ac.th>
2026-02-24 11:43:22 +0700

    commit 24/02

--- pokemon/data.py
+++ pokemon/data.py
@@ -1,8 +1,9 @@
+
 pokemon_types = [
-    'Water', 'Fire', 'Grass', 'Flying', 'Rock',
-    'Ground', 'Steel', 'Electric', 'Fairy', 'Ghost',
-    'Dark', 'Dragon', 'Ice', 'Bug', 'Fighting', 'Poison',
-    'Psychic', 'Normal'
+  'Water', 'Fire', 'Grass', 'Flying', 'Rock',
+  'Ground', 'Steel', 'Electric', 'Fairy', 'Ghost',
+  'Dark', 'Dragon', 'Ice', 'Bug', 'Fighting', 'Poison',
+  'Psychic', 'Normal'
 ]
 
 from pokemon.models import Type
